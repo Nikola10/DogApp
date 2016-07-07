2da46102169197b7215c40cb1d8992733394e754
Nikola10 <nikola.apple0801@gmail.com>
2016-07-07 23:32:58 +0200

    DoggApp2
    New

--- DogApp/FirstViewController.h
+++ DogApp/FirstViewController.h
@@ -1,6 +1,10 @@
 
 
+<<<<<<< Updated upstream
 #import <UIKit/UIKit.h>
+=======
+@interface FirstViewController :UITableView< UITableViewDataSource, UITableViewDelegate>
+>>>>>>> Stashed changes
 
 @interface FirstViewController : UIViewController<UITableViewDataSource, UITableViewDelegate>
 
